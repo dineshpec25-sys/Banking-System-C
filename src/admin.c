512f63d2b3b5a8dbb0e24b71a7b0bcb357c3386d
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-05-26 18:46:00 +0530

    Added the skeleton for Admin

---
+++ src/admin.c
@@ -0,0 +1,43 @@
+#include "../include/customer.h"
+
+void all_customer_details()
+{
+    printf("All Customer Details\n");
+
+    return ;
+}
+
+void customer_search_acc_num(int account_num)
+{
+    printf("Account Number\n");
+
+    return ;
+}
+
+void customer_search_customer_name(char *use_name)
+{
+    printf("Customer Name\n");
+
+    return ;
+}
+
+void delete_customer(int account_num)
+{
+    printf("Delete Customer\n");
+
+    return ;
+}
+
+void modify_cus_details(int account_num)
+{
+    printf("Modify Customer Details\n");
+
+    return ;
+}
+
+void bank_balance()
+{
+    printf("Bank Balance\n");
+
+    return ;
+}
